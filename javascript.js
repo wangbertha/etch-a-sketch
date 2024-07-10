82d6aad04e35fceea811f503cb02b84357c5abe0
wangbertha <wangbertha@gmail.com>
2024-07-10 01:25:29 -0500

    Turn grid div gray when mouse passes over
    Add event listener for mouseover to add gray background style

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ for (let i=0; i<16; i++) {
     for (let j=0; j<16; j++) {
         const ele = document.createElement('div');
         ele.classList.add('element');
+        ele.addEventListener('mouseover', () => ele.classList.add('touched'));
         row.appendChild(ele);
     }
     grid.appendChild(row);
