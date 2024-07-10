ad7eb403d9fb59291dac53dda8f17072a747c424
wangbertha <wangbertha@gmail.com>
2024-07-10 01:33:42 -0500

    Add user input to change grid size
    After user input, clear grid before rebuilding with new size

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
 const grid = document.querySelector('.grid');
-for (let i=0; i<16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j=0; j<16; j++) {
-        const ele = document.createElement('div');
-        ele.classList.add('element');
-        ele.addEventListener('mouseover', () => ele.classList.add('touched'));
-        row.appendChild(ele);
+
+const initialGridSize = 16;
+setGrid(initialGridSize);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const newGridSize = prompt('Enter width and height size: ');
+    setGrid(newGridSize);
+})
+
+function setGrid(size) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
     }
-    grid.appendChild(row);
+    for (let i=0; i<size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j=0; j<size; j++) {
+            const ele = document.createElement('div');
+            ele.classList.add('element');
+            ele.addEventListener('mouseover', () => ele.classList.add('touched'));
+            row.appendChild(ele);
+        }
+        grid.appendChild(row);
+    }
 }
